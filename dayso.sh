76f2b0260c966e8df609b116a727c7bc918a6947
drcongtri <dr.congtri@gmail.com>
2017-04-21 10:08:58 +0700

    Hoàn thành

--- dayso.sh
+++ dayso.sh
@@ -1,6 +1,6 @@
 
 i=0
-echo "Nhập một số bất kỳ: "
+echo -n "Nhập một số bất kỳ: "
 read n
 s=0
 while [[ $i -lt $n ]]
@@ -13,12 +13,54 @@ do
 done
 
 #xuat day 
+echo -n "     Dãy số vừa nhập là: "
 i=0
 while [[ $i -lt $n ]]
 do
-	echo -n "${arr[$i]} "
+	echo -n "${arr[$i]}  "
 	i=$(($i+1))
 
 done
-echo -e "\n Tổng các số trong dãy vừa nhập là $s"
+echo -e "\n     Tổng các số trong dãy vừa nhập là $s"
+echo -n "     Dãy tăng dần là: "
+for (( i = 0; i < $(($n-1)) ; i++ ))
+do
+   for (( j = $i; j < $n; j++ ))
+   do
 
+      if [[ ${arr[$i]} -gt ${arr[$j]} ]]
+      then
+
+       t=${arr[$i]}
+       arr[$i]=${arr[$j]}
+       arr[$j]=$t
+      fi
+   done
+done
+
+for (( i = 0; i < $n ; i++ ))
+do
+echo -n "${arr[$i]}  "
+done
+echo -e 
+echo -n "     Dãy giảm dần là: "
+for (( i = 0; i < $(($n-1)) ; i++ ))
+do
+   for (( j = $i; j < $n; j++ ))
+   do
+
+      if [[ ${arr[$i]} -lt ${arr[$j]} ]]
+      then
+
+       t=${arr[$i]}
+       arr[$i]=${arr[$j]}
+       arr[$j]=$t
+      fi
+   done
+done
+
+for (( i = 0; i < $n ; i++ ))
+do
+echo -n "${arr[$i]}  "
+done
+echo -e 
